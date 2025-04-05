0fd715fab1de1e2abdf65ca54d5dbdc3f5e346eb
zlonast <zlonast3@gmail.com>
2025-04-05 20:34:49 +0300

    update build-foundation-pak-for-ghc-9.10.1

--- build-foundation-pak-for-ghc-9.10.1.sh
+++ build-foundation-pak-for-ghc-9.10.1.sh
@@ -1,8 +1,9 @@
 set -x -e
+# set -u
+# set -o pipefail
 
 install_ghc_build_tools() {
-  stack --resolver lts-20.24 install alex-3.5.1.0 zip-cmd-1.0.1
-  read -p "install ghc 9.6.6 then press ENTER"
+  cabal install --overwrite-policy=always alex-3.5.1.0 zip-cmd-1.0.1
 }
 
 download_ghc_source_code() {
@@ -17,15 +18,6 @@ unpack_ghc_source_code_and_add_hadrian_to_git() {(
   set +e
   git add *
   git commit -am "original"
-)}
-
-build_stage2_ghc() {(
-  # does not compile with ghc 9.10.1
-  # let's try ghc 9.6.6 - it works ; USE THIS VERSION
-  cd ghc-9.10.1
-  ./boot.source
-  ./configure
-  hadrian/build-stack -j
 )}
 
 patch_hadrian_source_code() {(
@@ -45,6 +37,30 @@ patch_hadrian_source_code() {(
   git am < ../../../../patch/ghc-9.10.1/Cabal-3.10.3.0/0001-wpc.patch
 )}
 
+build_stage2_ghc() {(
+  # does not compile with ghc 9.10.1
+  # let's try ghc 9.6.6 - it works ; USE THIS VERSION
+  cd ghc-9.10.1
+  ./boot.source
+  ./configure
+  hadrian/build-stack -j
+)}
+
+build_wpc_plugin_with_stage2_ghc() {(
+  STAGE2_GHC=`pwd`/ghc-9.10.1/_build/stage1/bin/ghc
+  git clone https://github.com/zlonast/ghc-whole-program-compiler-project.git
+  git checkout 097745f5515178d44cbf84aea301b444945e9d89
+  cd ghc-whole-program-compiler-project/wpc-plugin
+
+  echo "packages: *.cabal external-stg-syntax" > cabal.project
+  echo "with-compiler: $STAGE2_GHC" >> cabal.project
+
+  # ghcup set 9.10.1
+  cabal build
+  # ghcup set 9.6.6
+  ln -s `find . -type f -name 'libwpc-plugin.so' -o -name 'libwpc-plugin.dylib' -o -name 'libwpc-plugin.dll' | head -1`
+)}
+
 patch_hadrian_set_final_stage_to_stage3() {(
   cd ghc-9.10.1/hadrian
   git am < ../../../patch/ghc-9.10.1/hadrian/0002-set-final-stage-to-stage3.patch
@@ -58,22 +74,10 @@ build_stage3_ghc_with_wpc_plugin() {(
   hadrian/build-stack -j foundation-pak --docs=none "$WPC_PLUGIN_GHC_OPTS"
 )}
 
-build_wpc_plugin_with_stage2_ghc() {(
-  STAGE2_GHC=`pwd`/ghc-9.10.1/_build/stage1/bin/ghc
-  git clone https://github.com/grin-compiler/ghc-whole-program-compiler-project.git
-  cd ghc-whole-program-compiler-project/wpc-plugin
-  # wpc-plugin for GHC 9.10.1
-  git checkout b55126d3360346708ae0ae4abf82fa77adac7ce5
-
-  echo "packages: *.cabal external-stg-syntax" > cabal.project
-  echo "with-compiler: $STAGE2_GHC" >> cabal.project
-
-  cabal build
-  ln -s `find . -type f -name 'libwpc-plugin.so' -o -name 'libwpc-plugin.dylib' -o -name 'libwpc-plugin.dll' | head -1`
-)}
-
 mkdir -p foundation-pak-ghc-9.10.1-wpc
 cd foundation-pak-ghc-9.10.1-wpc
+
+ghcup set ghc 9.6.6
 
 install_ghc_build_tools
 download_ghc_source_code
@@ -84,8 +88,9 @@ build_wpc_plugin_with_stage2_ghc
 patch_hadrian_set_final_stage_to_stage3
 build_stage3_ghc_with_wpc_plugin
 
-############
-# output foundation pak and ghc-9.2.7-wpc bindist
-############
+# ghcup set 9.10.1
+
+echo "output foundation pak and ghc-9.10.1 wpc bindist"
 
 ls -lah `pwd`/ghc-9.10.1/_build/foundation-pak/*.tar.*
+ls -lah `pwd`/ghc-9.10.1/_build/bindist/ghc-9.10.1-x86_64-unknown-linux/bin/
